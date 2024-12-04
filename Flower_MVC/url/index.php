12b8afbcc1e57ebc07b5ed3ac43ce2a85b833d88
giang <vutruonggiang20420051@gmail.com>
2024-12-04 11:30:44 +0700

    jj

--- Flower_MVC/url/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once '../core/Router.php';
-require_once '../controllers/FlowerController.php';
-
-$router = new Router();
-$router->add('/url', [new FlowerController(), 'index']);
-$router->dispatch($_SERVER['REQUEST_URI']);
-
-?>
